71f4cd442d8d6cf171b2ba64b8280bfcac3fb9b9
Ankita Saha <76600603+ankita320@users.noreply.github.com>
2022-02-13 01:32:18 -0500

    Update script.js

--- draft/script.js
+++ draft/script.js
@@ -29,19 +29,34 @@ groupMembership = AdminDirectory.user.remove; //(?)
 SpreadsheetApp.getActiveSpreadsheet().rg.getRow(), ev.user.getEmail
 SpreadsheetApp.getGroups().getInactiveUser().bySheetName 
 //(?)
-GroupsApp.getGroups().getInactiveUser().bySheetName //<-- grabbing user from group
-var dest_sheet = spreadsheet.getEmail(dest_sheet_name)
-var dest_range = sheet.getEmail();
+var group = AdminDirectory.getEmail(dest_sheet_name);
+
+var groups = GroupsApp.getGroups().getInactiveUser().bySheetName(dest_sheet_name) //<-- grabbing user from group
+  var dest_sheet = spreadsheet.getEmail(dest_sheet_name)
+  var dest_range = sheet.getEmail();
+//move user to group
+
+
+date = Utilities.formatDate(new Date(), "GMT-8", "HH:mm:ss MM/dd/yyyy")
+dest_range = dest_sheet.getRange(emptyRow, 1, 1, 1)
+name_range = dest_sheet.getRange(emptyRow, 1, 1, 2)
+console.log("Note: " + note)
+if(note != "") note = note.concat("\n\n")
+dest_range.setNote(note + "Moved by " + usr + " at " + date + "in PT\n")
+name_range.setBackgrounds([[color, color]])
+//^^ code for moving to group in spreadsheet(apps script)
+
+var dest_range = groups.remove(user);
+
 //not sure if below code is necessary
 dest_range.shiftRowGroupDepth();
 //-----
-var group = AdminDirectory.getEmail(dest_sheet_name);
 
 
 var dest_range = dest_sheet.getRange(emptyRow);
 
 // removes group
-var dest_range = group.remove();
+var dest_range = groups.remove();
 
 AdminDirectory.toast(`Removing user from ${dest_range} group, please wait...` 2)
 
