4c6c0fba803d09837e655e4838fbb8a2477b3aae
Ankita Saha <76600603+ankita320@users.noreply.github.com>
2022-07-09 21:31:10 -0400

    Update script.js

--- draft/script.js
+++ draft/script.js
@@ -1,37 +1,25 @@
 
 
-
-userEmail = userDataRaw[6];
-
-if (dest_sheet_name === "Teacher Inactive") {
-   groupEmail = "teachers@roboticsforall.net";}
-
-else if (dest_sheet_name === "Curriculum Inactive") {
-   groupEmail = "curricdev@roboticsforall.net";}
-
-else () {
-   //do nothing
-}
-
-
-
-
-
-if (AdminDirectory.Members.hasMember(groupEmail,userEmail).isMember)
-   AdminDirectory.Members.remove(groupEmail, userEmail);)
-
-
-
+function updateGroupMembership(userDataRaw, dest_sheet_name)
 {
+
+
+  msg = ""
+ 
   var userEmail = userDataRaw[8];
   Logger.log("User to move: " + userDataRaw[8])
   var groupEmail = ""
 
   if (dest_sheet_name === "Teacher Inactive") 
-  {groupEmail = "teachers@roboticsforall.net"}
+  {
+    groupEmail = "teachers@roboticsforall.net"
+  }
   else if (dest_sheet_name === "Curriculum Inactive") 
-  {groupEmail = "curricdev@roboticsforall.net"}
- if (groupEmail != "") 
+  {
+    groupEmail = "curricdev@roboticsforall.net"
+  }
+
+  if (groupEmail != "") 
   {
 
     try 
@@ -39,24 +27,25 @@ if (AdminDirectory.Members.hasMember(groupEmail,userEmail).isMember)
       
     if (AdminDirectory.Members.hasMember(groupEmail,userEmail).isMember) 
       {
-    // SpreadsheetApp.getUi().alert(userEmail + " has been successfully removed from " + groupEmail);
-      SpreadsheetApp.getActiveSpreadsheet().toast('User has been successfully removed');
-        _msg = "hi"
+        SpreadsheetApp.getActiveSpreadsheet().toast('User will be removed');
+        msg = userEmail + " can be removed from " + groupEmail + "."
       } else {
-        SpreadsheetApp.getUi().alert("Error: " + userEmail + " is not a member of " + groupEmail);
-        _msg = "hi1"
+      SpreadsheetApp.getUi().alert("Error: " + userEmail + " is not a member of " + groupEmail);
+        msg = "⚠️ Error: " + userEmail + " is not a member of " + groupEmail + "."
       }
 
     } catch (err)
     {
-      Logger.log(err)
-
       SpreadsheetApp.getUi().alert("Error: An error occured in trying to update the User's group membership. Please remove " + userEmail + " from " + groupEmail + " manually. The error has been logged in the execution log.");
-      _msg = "hello"
+      msg = userEmail + " can be removed from " + groupEmail + "."
     }
 
   } else {
     SpreadsheetApp.getUi().alert("Please manually make " + userEmail + " inactive.");
-    _msg = "hello2"
-  }}
-      
+    msg = "Please manually make " + userEmail + " from " + groupEmail + " inactive."
+  }
+
+  sendSlack(overseerChannel, `{"text": "${msg}", "post_at": 1657339200}`);
+
+}
+/*
